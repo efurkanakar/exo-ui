9daf6178d90ce60b3c5aafd7b2f3892fa6c32b1e
Furkan Akar <efurkanakar@gmail.com>
2025-09-27 19:54:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-declare const process: {
-  env: Record<string, string | undefined>
-}
-
 const repositoryPath = process.env.GITHUB_REPOSITORY?.split('/')[1]
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:  base: '/exo-ui/,
+  base: repositoryPath ? `/${repositoryPath}/` : '/',
   plugins: [react()],
 })

8d18dee97471f56ba5c0e4876e862997ad4255f1
Furkan Akar <efurkanakar@gmail.com>
2025-09-27 20:16:28 +0300

    Set GitHub Pages base fallback

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import packageJson from './package.json' assert { type: 'json' }
+
 declare const process: {
   env: Record<string, string | undefined>
 }
 
-const repositoryPath = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const fallbackRepositoryPath = typeof packageJson.name === 'string' ? packageJson.name : undefined
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: repositoryPath ? `/${repositoryPath}/` : '/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const repositoryPath =
+    process.env.GITHUB_REPOSITORY?.split('/')?.[1] ?? fallbackRepositoryPath
+
+  return {
+    base: command === 'serve' ? '/' : repositoryPath ? `/${repositoryPath}/` : '/',
+    plugins: [react()],
+  }
 })

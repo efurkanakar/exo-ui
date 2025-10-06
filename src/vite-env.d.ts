0545c098704c66a50175b1a19613890192dbd75f
efurkanakar <efurkanakar@gmail.com>
2025-10-06 19:13:27 +0000

    fix: change vite file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ADMIN_API_KEY?: string;
+  readonly [key: string]: string | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
